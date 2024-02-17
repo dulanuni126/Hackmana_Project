94abdb107adbda65abb499be2645e10dbd1b1e19
Sanjaya Pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-02-17 15:04:18 +0530

    connected scene
    Conncet scene to the nav panel and load them when clicked it

--- src/main/java/org/example/hakmana/NavPanelController.java
+++ src/main/java/org/example/hakmana/NavPanelController.java
@@ -7,11 +7,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.io.IOException;
 import java.net.URL;
@@ -19,6 +23,9 @@ import java.util.ResourceBundle;
 
 
 public class NavPanelController extends AnchorPane implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     /*-------variables for collapse the navigation panel----------*/
     @FXML
@@ -106,6 +113,41 @@ public class NavPanelController extends AnchorPane implements Initializable {
             test.set(false);
         }
         isCollapsed = !isCollapsed;
+    }
+    public void deviceMnagmnt(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene/DeviceMngmnt.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void overviewScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene/Overview.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void userMngmntScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene/UserMngmnt.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void reportHndlingScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene/ReportHndling.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void dashboardScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene/dashboard.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- src/main/java/org/example/hakmana/PathFinderController.java
+++ src/main/java/org/example/hakmana/PathFinderController.java
@@ -1,8 +1,10 @@
 package org.example.hakmana;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -10,6 +12,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PathFinderController extends VBox implements Initializable {
+
+    @FXML
+    private Button backBttn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -27,5 +33,9 @@ public class PathFinderController extends VBox implements Initializable {
         catch(IOException e){
             throw new RuntimeException(e);
         }
+    }
+
+    public void goBack(){
+        System.out.println("Go back button pressed");
     }
 }
