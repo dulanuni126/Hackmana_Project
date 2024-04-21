86eef27a93fa68c417334bff4b736e66fd776971
uditha <udithaimesh4@gmail.com>
2024-04-21 15:15:46 +0530

    Revert "create auto update cards"
    This reverts commit 915f039b62a6de160a1f29fef2412bb10d7a8eac.

--- src/main/java/org/example/hakmana/LoginPageController.java
+++ src/main/java/org/example/hakmana/LoginPageController.java
@@ -11,10 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import org.example.hakmana.model.DatabaseConnection;
 
 import java.io.IOException;
-import java.sql.*;
 
 public class LoginPageController {
     private Stage stage;
